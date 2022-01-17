6869fc9b3b1cee8bd8870c9896ab888075a2d8d9
Jur9x <60826133+Jur9x@users.noreply.github.com>
2022-01-17 22:55:30 +0100

    1.1 Přidán časovač na aimtrain

--- js/aimtrain.js
+++ js/aimtrain.js
@@ -12,6 +12,11 @@ let text = 1;
 let skore = 0;
 let skoreA = 0;
 let oldskore = 0;
+let timep = 0;
+
+var start;
+var end;
+var time;
 
 //center canvas
 function centerCanvas() {
@@ -52,14 +57,17 @@ function draw() {
     if (promena == 0) {
         textSize(50);
         fill("black");
-        canvas.text("Game over!", width / 2 - 140, 150);
-        canvas.text(`Your score is ${oldskore}`, width / 2 - 170, 200);
-        canvas.text("Click on purple circle to play again!", width / 2 - 380, 250);
+        canvas.text("Game over!", width / 2 - 140, 220);
+        canvas.text(`Your score is ${oldskore}`, width / 2 - 170, 270);
+        canvas.text("Click on purple circle to play again!", width / 2 - 380, 320);
         mainc.x = width / 2;
         mainc.y = height / 2;
         fill("purple");
         mainc.draw();
         inrskore.innerHTML = ``
+    }
+    if (timep == 1) {
+        inrskore.innerHTML = `${skore} (${time} ms)`
     }
     if (skoreA == 1) {
         inrskore.innerHTML = `${skore}`
@@ -73,6 +81,10 @@ function draw() {
 
 //reakce na kliknutí
 function mousePressed() {
+    if (skoreA == 1) {
+        timep = 1;
+        skoreA = 2;
+    }
     if (skoreA == 0) {
         skoreA = 1;
     }
@@ -83,11 +95,16 @@ function mousePressed() {
         skore = 0;
     }
     if (d < mainc.p / 2) {
+        end = new Date().getTime();
+        time = end - start;
+        console.log(time + " milisekund");
         mainc.x = random(0, width);
         mainc.y = random(0, height);
         skore++;
+        start = new Date().getTime();
     } else {
         promena = 0;
         skoreA = 0;
+        time = 0;
     }
 }
